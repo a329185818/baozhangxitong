9c3a3b4ca966fcbc9cbb1b3124805e0e19b8099d
hfx598708047 <598708047@qq.com>
2019-05-15 17:52:21 +0800

    受理1, 扫描2, 复审3, 审核4, 配房5, 归档6 1.新需求为：派件给A，A拥有审批权限，A结束审批后权限重置，由boss重新派件配房权限，所以A可能没有配房权限。为了兼容另外系统权限，也就是说，初始的管理员都只有1-4的审批权限

--- guns-admin/src/main/java/com/stylefeng/guns/modular/support/service/impl/AuthorityServiceImpl.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/support/service/impl/AuthorityServiceImpl.java
@@ -6,7 +6,6 @@ import com.stylefeng.guns.modular.support.service.AuthorityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,8 +42,12 @@ public class AuthorityServiceImpl implements AuthorityService {
         map.put("USERNUM",userId);
         map.put("OPTYPENUM","81");
         map.put("OPUSERROLE","1");
+        //受理1, 扫描2, 复审3, 审核4, 配房5, 归档6
+        //新需求为，派件给A，A拥有审批权限，A结束审批后权限重置，由boss重新派件配房权限，所以A可能没有配房权限
+        //也就是说，初始的管理员都只有1-4的审批权限
+        //TODO:sql语句有时间在优化
         for(int i = 1;i<7;i++){
-            for(int y = 1;y<7;y++){
+            for(int y = 1;y<5;y++){
                 map.put("OPPARTNUM","31"+i);
                 map.put("OPUSERPOWER",""+y);
                 authorityMapper.addUserAuthority(map);
